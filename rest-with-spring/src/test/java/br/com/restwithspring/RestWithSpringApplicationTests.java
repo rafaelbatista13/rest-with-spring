5b12f75ddce16424d0c9156cf1ea13874a604393
rafaelbatista13 <rafaelbatista11122@gmail.com>
2024-03-18 18:41:56 -0300

    First steps on creating and consuming api's

---
+++ rest-with-spring/src/test/java/br/com/restwithspring/RestWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
