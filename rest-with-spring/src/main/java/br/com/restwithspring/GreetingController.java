81e5e296aa74ffe837af35870045ecf67b78cad2
rafaelbatista13 <rafaelbatista11122@gmail.com>
2024-03-18 19:04:18 -0300

    hotfix requestmapping

--- rest-with-spring/src/main/java/br/com/restwithspring/GreetingController.java
+++ rest-with-spring/src/main/java/br/com/restwithspring/GreetingController.java
@@ -1,18 +1,17 @@
 package br.com.restwithspring;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
